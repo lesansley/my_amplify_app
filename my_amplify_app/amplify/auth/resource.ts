572046e4c390ff3affdfe08e92af5ecef2e01e02
Les Ansley <les@ansleyfamily.com>
2024-07-29 12:57:54 +0200

    Project setup

---
+++ my_amplify_app/amplify/auth/resource.ts
@@ -0,0 +1,38 @@
+import { defineAuth, secret } from "@aws-amplify/backend";
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+        scopes: ["email"],
+        attributeMapping: {
+          email: "email",
+        },
+      },
+      facebook: {
+        clientId: secret("FACEBOOK_CLIENT_ID"),
+        clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
+        scopes: ["email"],
+        attributeMapping: {
+          email: "email",
+        },
+      },
+      callbackUrls: [
+        "http://localhost:3000/profile",
+        "https://mywebsite.com/profile",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://cliqueof10.com"],
+    },
+  },
+  multifactor: {
+    mode: "OPTIONAL",
+    totp: true,
+  },
+});
